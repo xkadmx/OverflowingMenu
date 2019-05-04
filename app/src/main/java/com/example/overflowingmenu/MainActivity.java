b9253dc4186653399b6aeeb2c9ac9662a9841d27
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-04 19:53:20 +0200

    in onOptionsItemSelected method we build a main_view object of CoordinatorLayout
    in onOptionsItemSelected we set parameter item
    
    if user checks the item of a given color it will change the Background color on main_View object (CoordinatorLayout)
    
    Signed-off-by: xkadmx <zuzanna.rosinska@yahoo.com>

--- app/src/main/java/com/example/overflowingmenu/MainActivity.java
+++ app/src/main/java/com/example/overflowingmenu/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.overflowingmenu;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -37,16 +39,48 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        CoordinatorLayout main_view = (CoordinatorLayout) findViewById(R.id.main_view);
+        switch(item.getItemId()) {
+            case R.id.menu_red:
+                if (item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                main_view.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.menu_green:
+                if (item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                main_view.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.menu_yellow:
+                if (item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                main_view.setBackgroundColor(Color.YELLOW);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+
+
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
